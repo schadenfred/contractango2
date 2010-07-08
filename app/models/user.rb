9f0548d11b61a45f6e31ea0b5401f656eedb2375
Fred Schoeneman <fred.schoeneman@gmail.com>
2010-07-08 14:52:44 -0700

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,8 @@ class User < ActiveRecord::Base
 
    before_save :encrypt_password
 
+   default_scope :order => 'users.created_at'
+
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -44,7 +44,7 @@ describe UsersController do
     end
   end
 
-describe "POST 'create'" do
+  describe "POST 'create'" do
 
     describe "failure" do
 
@@ -64,6 +64,27 @@ describe "POST 'create'" do
       it "should render the 'new' page" do
         post :create, :user => @attr
         response.should render_template('new')
+      end
+    end
+
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com",
+                  :password => "foobar", :password_confirmation => "foobar" }
+        @user = Factory(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(true)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to contractango/i
       end
     end
   end
